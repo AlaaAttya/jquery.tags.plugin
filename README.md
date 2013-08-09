8157ab965a23a9b89b65115263f4859dda83b95e
AlaaAttya <alaaattya@mint>
2013-08-10 01:28:49 +0200

    Added: Plugin description
    Signed-off-by: AlaaAttya <alaaattya@mint>

--- README.md
+++ README.md
@@ -2,3 +2,39 @@ jquery.tags.plugin
 ==================
 
 ### A jquery plugin which will help developers to visualize their tags
+
+- You can simple use this plgin to visualize your tags in a neat UI
+
+- You can just do so with only a single line of code, just apply this code to 
+
+  *editable div* just like the following example
+  
+  <code>
+
+            $("#test").tags({
+                stylingClass: "selector",
+                valueSelector: "#tabs"
+            });
+
+
+  </code>
+
+  - *test* is the id ('or even the class') of the edtable div
+  - *stylingClass* is the style which will be applied to each tag container('*it must be a class*')
+  - *valueSelector* is the selector where you can get the tags value from 
+    ('actually it is a hidden field')
+
+
+### Hint
+
+ - You can get your tags values in a single array by using this snippet
+   
+   <code>
+	var tags_arr = $('valueSelector').split("#");
+	//removes the first emty element as the string starts with the delimiter
+        tags_arr.shift();
+   </code>
+ - Hope you'll love it, you can use the attached example as your guide
+
+   (*Don't forget to include the jQuery library at the top and add your own style for 'stylingClass'*)
+
--- jquery.tags.plugin.js
+++ jquery.tags.plugin.js
@@ -1,82 +1,8 @@
 /**
  * @author Alaa Attya Mohamed <alaa.attya@pngine.com>
- * 
  * */
-(function($) {
-    //@todo  fix backspace event
-    //append tags
-    $.fn.tags = function(options) {
 
-        var settings = $.extend({
-            stylingClass: "highlight",
-            valueSelector: "content"
-        }, options);
-        
- 
-        
-        $(this).attr('contenteditable', 'true');
-        if (settings.valueSelector.charAt(0) === "#")
-            $(this).before("<input type='hidden' id='" + settings.valueSelector.substr(1, settings.valueSelector.length) + "'>");
-        else
-            $(this).before("<input type='hidden' class='" + settings.valueSelector + "'>");
+(function($){$.fn.tags=function(options){var settings=$.extend({stylingClass:"highlight",valueSelector:"content"},options);$(this).attr("contenteditable","true");if(settings.valueSelector.charAt(0)==="#")$(this).before("<input type='hidden' id='"+settings.valueSelector.substr(1,settings.valueSelector.length)+"'>");else $(this).before("<input type='hidden' class='"+settings.valueSelector+"'>");$(this).keydown(function(event){switch(event.keyCode){case 8:if($.trim(decodeURI($(this).clone().children().remove().end().text())).length===
+0)if($(this).children().length>0){$("."+settings.stylingClass).last().remove();return false}break;case 13:return false;break;case 32:var origin=$(this).children();var text=$.trim(decodeURI($(this).clone().children().remove().end().text()));if(text.length>0){$(this).html("");$(this).text("");if($(this).size()>0){$(this).append(origin);$(this).append("<span contenteditable=false class='"+settings.stylingClass+"' style='position:relative;margin-top:-3px;float:left;margin-right:5px'>"+text+"</span>")}else $(this).html("<span contenteditable=false class='"+
+settings.stylingClass+"' style='position:relative;margin-top:-3px;float:left;margin-right:5px'>"+text+"</span>");if(settings.valueSelector.charAt(0)==="#")$(settings.valueSelector).val($(settings.valueSelector).val()+" "+text);else $("."+settings.valueSelector).val(("."+settings.valueSelector).val()+" "+text)}break}})}})(jQuery);
 
-        //set content value with editable div value
-        //if 'space' key was pressed, set an element
-        $(this).keydown(function(event) {
-
-            switch (event.keyCode) {
-
-                //backspace
-                case 8:
-                    if ($.trim(decodeURI($(this).clone().children().remove().end().text())).length === 0) {
-                        if ($(this).children().length > 0) {
-
-                            $("." + settings.stylingClass).last().remove();
-                            return false;
-                        }
-                    }
-                    break;
-
-                    //enter key    
-                case 13:
-                    return false;
-                    break;
-                    //space key
-                case 32:
-
-                    //get only children nodes
-                    var origin = $(this).children();
-
-                    //get only text
-                    var text = $.trim(decodeURI($(this).clone().children().remove().end().text()));
-
-
-                    if (text.length > 0) {
-
-                        $(this).html('');
-                        $(this).text('');
-
-                        if ($(this).size() > 0) {
-
-                            $(this).append(origin);
-                            $(this).append("<span contenteditable=false class='" + settings.stylingClass + "' style='position:relative;margin-top:-3px;float:left;margin-right:5px'>" + text + "</span>");
-
-                        } else {
-
-                            $(this).html("<span contenteditable=false class='" + settings.stylingClass + "' style='position:relative;margin-top:-3px;float:left;margin-right:5px'>" + text + "</span>");
-                        }
-                        
-                        
-                        if (settings.valueSelector.charAt(0) === "#")
-                            $(settings.valueSelector).val($(settings.valueSelector).val() + " " + text);
-                        else
-                            $("." + settings.valueSelector).val(("." + settings.valueSelector).val() + " " + text);
-
-                    }
-
-                    break;
-            }
-        });
-        
-    }
-}(jQuery));
